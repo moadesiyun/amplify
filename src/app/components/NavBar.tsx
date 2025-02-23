caad9f5ad62a07e781724f155aecf67d51d16843
Mo Adesiyun <8804eply.github.com3947+moadesiyun@usersensions.noreply.github.com>
2025-02-23 15:39:27 -0600

    navigation

--- src/app/components/NavBar.tsx
+++ src/app/components/NavBar.tsx
@@ -1,8 +1,37 @@
 import React from 'react'
-
+import Image from 'next/image'
+import logo from '../assets/images/logo.png'
+import { relative } from 'path'
 const NavBar = () => {
   return (
-    <div>NavBar</div>
+    <nav>
+      <div className='flex flex-1 items-center justify-center md:items-stretch md:justify-start'>
+          <a
+            className='flex flex-shrink-0 items-center mr-4'
+            >
+              <Image src={logo} width={50} objectPosition='relative' objectFit='contains' alt='Custom Rug Maker' />
+              <span className='hidden md:block text-white text-2xl font-bold ml-2'>
+                Rugged Tufts.
+              </span>
+            </a>
+            
+          <a href='/index.html'
+          className='text-white bg-purple hover:bg-purple-900 hover:text-white rounded-md px-3 py-2'
+          >
+            Home
+          </a>
+        <a href='/index.html'
+          className='text-white hover:bg-purple-900 hover:text-white rounded-md px-3 py-2'
+          >
+            Products
+          </a>
+        <a href='/index.html'
+          className='text-white hover:bg-purple-900 hover:text-white rounded-md px-3 py-2'
+          >
+            Contacts
+          </a>    
+      </div>
+    </nav>
   )
 }
 
