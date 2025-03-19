ff3e2aca05de010626fb9aeb28249a080d54e3af
Mo Adesiyun <8804eply.github.com3947+moadesiyun@usersensions.noreply.github.com>
2025-03-19 13:50:19 -0500

    form

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,12 +27,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <NavBar />
       <body>
       <div className="bg-purple-300 grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <NavBar />
+      
       {children}
-      <Footer />
+      
           </div></body>
+          <Footer />
     </html>
         
   );
