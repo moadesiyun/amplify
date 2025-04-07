2d370d1785d3b63c335aaafd38b00f2817750740
Mo Adesiyun <8804eply.github.com3947+moadesiyun@usersensions.noreply.github.com>
2025-04-06 21:27:07 -0500

    bg color

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
     <html lang="en">  
        
        <body>
-        <div className="bg-purple-300 grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+        <div className="bg-violet-900 grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
        <NavBar className="w-full max-w-6xl"/>    
        {children} 
        <Footer />
